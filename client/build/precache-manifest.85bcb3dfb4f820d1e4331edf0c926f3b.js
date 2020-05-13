4e35fd07b290fe5d73dec177a0ea1a254db2fce6
Sami Bergman <sami.bergman@outlook.com>
2020-05-13 09:41:03 +0300

    joku loppusilaus

---
+++ client/build/precache-manifest.85bcb3dfb4f820d1e4331edf0c926f3b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae7d4e0d07faa0d8d7c3c85d44f0dd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "901e5224725075be35da",
+    "url": "/static/css/main.113ee730.chunk.css"
+  },
+  {
+    "revision": "15f86084880b9d535068",
+    "url": "/static/js/2.7fd695e6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.7fd695e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "901e5224725075be35da",
+    "url": "/static/js/main.7629a05e.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "98c39b65d93ef832a8da108df595720a",
+    "url": "/static/media/Flaticon.98c39b65.woff"
+  },
+  {
+    "revision": "bdfe38ffec43dd2ec50c009674939390",
+    "url": "/static/media/placeholder.bdfe38ff.png"
+  },
+  {
+    "revision": "dbe2ef148fa0f8874e95073683ff7e1e",
+    "url": "/static/media/shopping.dbe2ef14.svg"
+  }
+]);
